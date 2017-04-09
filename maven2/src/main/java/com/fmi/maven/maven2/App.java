cd4c0acb0522f99d8f88de1412aa6e5b0c10fec5
fmi <fmi@fmi-535-11.fmi.lcl>
2017-04-09 10:34:47 +0300

    hello, this is m first commit

---
+++ maven2/src/main/java/com/fmi/maven/maven2/App.java
@@ -0,0 +1,15 @@
+package com.fmi.maven.maven2;
+
+import com.fmi.maven.maven1.Class1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new Class1().printMe(null);
+    }
+}
