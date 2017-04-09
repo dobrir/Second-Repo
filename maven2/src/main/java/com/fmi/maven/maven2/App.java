38496d58547d11828429789f2d55ba308dc9da2c
fmi <fmi@fmi-535-11.fmi.lcl>
2017-04-09 12:02:07 +0300

    later changed null to master string

--- maven2/src/main/java/com/fmi/maven/maven2/App.java
+++ maven2/src/main/java/com/fmi/maven/maven2/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
 	{
-	    new Class1().printMe(null);
+	    new Class1().printMe("master later changed");
 	    //,jhb ghjk bhj k
 	    
 	    
