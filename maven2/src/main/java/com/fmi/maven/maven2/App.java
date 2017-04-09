ea2461cef4524b7f10db33d0dec60879368fe73b
fmi <fmi@fmi-535-11.fmi.lcl>
2017-04-09 12:00:41 +0300

    chnged to null

--- maven2/src/main/java/com/fmi/maven/maven2/App.java
+++ maven2/src/main/java/com/fmi/maven/maven2/App.java
@@ -13,11 +13,10 @@ public class App
 	    new Class1().printMe(null);
 	    //,jhb ghjk bhj k
 	    
-	    
-	    
-	    
-	    
 	    //hello worlsd
+	    
+	    
+	    //this later change to master branch
 	}
 
 
